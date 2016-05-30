0ba534bc0a20aad0591d95026716edce521449a5
mRHeisen <miguel.avilla92@gmail.com>
2016-05-30 02:28:25 -0300

    Adicionando novas telas

--- HappyFitnessApp/HappyFitnessApp/platforms/android/assets/www/js/app.js
+++ HappyFitnessApp/HappyFitnessApp/platforms/android/assets/www/js/app.js
@@ -1,25 +1,59 @@
 angular.module('starter', ['ionic'])
     .config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider
-        .state('home', {
+        .state('login', {
             url: '/',
+            templateUrl: 'login.html',
+            controller: 'loginController as vm'
+        })
+        .state('home', {
+            url: '/home',
             templateUrl: 'home.html',
             controller: 'homeController as vm'
         })
         .state('menu', {
             url: '/menu',
-            templateUrl: 'menu.html'
+            templateUrl: 'menu.html',
+            controller: "menuController as vm"
         })
         .state('treino', {
             url: '/treino',
             templateUrl: 'treino.html',
             controller: "treinoController as vm"
         })
+        .state('perfil', {
+            url: '/perfil',
+            templateUrl: 'perfil.html',
+            controller: "perfilController as vm"
+        })
+        .state('historico', {
+            url: '/historico',
+            templateUrl: 'historico.html',
+            controller: "historicoController as vm"
+        })
+        .state('exercicio', {
+            url: '/exercicio',
+            templateUrl: 'exercicio.html',
+            controller: "exercicioController as vm"
+        })
+        .state('endTreino', {
+            url: '/endTreino',
+            templateUrl: 'endTreino.html',
+            controller: "endTreinoController as vm"
+        })
+
 
         $urlRouterProvider.otherwise("/");
     })
+    .controller('loginController', loginController)
     .controller('homeController', homeController)
+    .controller('menuController', menuController)
     .controller('treinoController', treinoController)
+    .controller('perfilController', perfilController)
+    .controller('historicoController', historicoController)
+    .controller('exercicioController', exercicioController)
+    .controller('endTreinoController', endTreinoController)
+    
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(cordova.platformId === 'ios' && window.cordova && window.cordova.plugins.Keyboard) {
